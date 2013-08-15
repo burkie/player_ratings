110d67a15cd1e4eaf19dcaec963b4f16667af1f2
Liam Burke <hello@burk.ie>
2013-08-15 12:49:38 +0100

    Update rateanything.js

--- theme folder/js/rateanything.js
+++ theme folder/js/rateanything.js
@@ -1,7 +1,7 @@
 	$(document).ready(function() {
 		$('div').each(function() {
 			if(this.id.match('rate_')){
-				$(this).load('../rateanything.php?id='+this.id.replace('rate_',''));
+				$(this).load('///http://news.arseblog.com/rateanything.php?id='+this.id.replace('rate_',''));
 			}
 		});
 	});
@@ -9,7 +9,7 @@
 	function addRating(div_id,rating){
 	 $.ajax({
 	   type: "GET",
-	   url: "../rateanything.php",
+	   url: "//http://news.arseblog.com/rateanything.php",
 	   data: "id="+div_id+"&rating="+rating,
 	   success: function(data){
 		 $('#rate_'+div_id).hide().html(data).fadeIn('slow');
